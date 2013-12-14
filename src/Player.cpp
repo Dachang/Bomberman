210feee24c4dd100e319192346f22709451ae190
Ocean <dachang221@126.com>
2013-12-14 17:40:00 +0800

    Player类的basic control
    修改了转向的bug

---
+++ src/Player.cpp
@@ -0,0 +1,234 @@
+#include "Player.h"
+
+Player::Player(Ogre::Entity* entity,Ogre::SceneNode* node,Ogre::SceneManager *sceneMgr,Ogre::Camera *camera,OIS::Keyboard *keyboard)
+	:GameObject(entity,node,sceneMgr, camera, keyboard)
+{
+	Start();
+}
+Player::~Player(void)
+{
+
+}
+
+void Player::Start(void)
+{
+	_moveSpeed = 250;
+	_direction = UP_DIRECTION;
+	_isTurningVertical = 1;
+	_isTurningHorizontal = 1;
+
+	_leftToUp = 1;
+	_leftToRight = 1;
+	_leftToDown = 1;
+	_rightToDown = 1;
+	_rightToUp = 1;
+	_upToDown = 1;
+
+	_pos = START_POSITION;
+}
+void Player::Update(const Ogre::FrameEvent& evt)
+{
+	Ogre::Vector3 transVector = Ogre::Vector3::ZERO;
+	//get input
+	if (_Keyboard->isKeyDown(OIS::KC_RIGHT))
+	{
+		playerMoveRight();
+		//transVector.z += _isTurningHorizontal * _moveSpeed;
+		transVector.z -= _leftToRight * _moveSpeed;
+		transVector.z += _rightToUp * _moveSpeed;
+		transVector.z += _rightToDown * _moveSpeed;
+	}
+	else if (_Keyboard->isKeyDown(OIS::KC_LEFT))
+	{
+		playerMoveLeft();
+		//transVector.z -= _moveSpeed;
+		transVector.z += _leftToUp * _moveSpeed;
+		transVector.z += _leftToRight * _moveSpeed;
+		transVector.z += _leftToDown * _moveSpeed;
+	}
+	else if (_Keyboard->isKeyDown(OIS::KC_UP))
+	{
+		playerMoveUp();
+		//transVector.z -= _moveSpeed;
+		transVector.z -= _leftToUp * _moveSpeed;
+		transVector.z -= _rightToUp * _moveSpeed;
+		transVector.z += _upToDown * _moveSpeed;
+	}
+	else if (_Keyboard->isKeyDown(OIS::KC_DOWN))
+	{
+		playerMoveDown();
+		//transVector.z += _isTurningVertical * _moveSpeed;
+		transVector.z -= _leftToDown * _moveSpeed;
+		transVector.z -= _rightToDown * _moveSpeed;
+		transVector.z -= _upToDown * _moveSpeed;
+	}
+	else if (_Keyboard->isKeyDown(OIS::KC_SPACE))
+	{
+	}
+	//update
+	updateAnimation(evt);
+	this->_Node->translate(transVector * evt.timeSinceLastFrame,Ogre::Node::TS_LOCAL);
+	this->_pos = convertWorldPosToGridPos(this->_Node->getPosition());
+}
+
+void Player::playerMoveLeft()
+{
+	switch (_direction)
+	{
+	case RIGHT_DIRECTION:
+		this->_Node->yaw(Ogre::Degree(180));
+		_leftToRight = -1;
+		_leftToUp = 0;
+		_leftToDown = 0;
+		break;
+	case UP_DIRECTION:
+		this->_Node->yaw(Ogre::Degree(90));
+		_leftToRight = 0;
+		_leftToUp = -1;
+		_leftToDown = 0;
+		break;
+	case DOWN_DIRECTION:
+		this->_Node->yaw(Ogre::Degree(-90));
+		_leftToRight = 0;
+		_leftToUp = 0;
+		_leftToDown = -1;
+		break;
+	default:
+		break;
+	}
+	_direction = LEFT_DIRECTION;
+}
+
+void Player::playerMoveRight()
+{
+	switch(_direction)
+	{
+	case LEFT_DIRECTION:          
+		this->_Node->yaw(Ogre::Degree(180));
+		_leftToRight = 1;
+		_rightToDown = 0;
+		_rightToUp = 0;
+		//_isTurningHorizontal = -1;
+		break;
+	case UP_DIRECTION:
+		this->_Node->yaw(Ogre::Degree(-90));
+		_leftToRight = 0;
+		_rightToDown = 0;
+		_rightToUp = -1;
+		break;
+	case DOWN_DIRECTION:
+		this->_Node->yaw(Ogre::Degree(90));
+		_leftToRight = 0;
+		_rightToDown = -1;
+		_rightToUp = 0;
+		break;
+	default:
+		break;
+	}
+	_direction = RIGHT_DIRECTION;
+}
+
+void Player::playerMoveUp()
+{
+	switch(_direction)
+	{
+	case DOWN_DIRECTION:
+		this->_Node->yaw(Ogre::Degree(180));
+		_upToDown = -1;
+		_rightToUp = 0;
+		_leftToUp = 0;
+		break;
+	case LEFT_DIRECTION:
+		this->_Node->yaw(Ogre::Degree(-90));
+		_upToDown = 0;
+		_rightToUp = 0;
+		_leftToUp = 1;
+		break;
+	case RIGHT_DIRECTION:
+		this->_Node->yaw(Ogre::Degree(90));
+		_upToDown = 0;
+		_rightToUp = 1;
+		_leftToUp = 0;
+		break;
+	default:
+		break;
+	}
+	_direction = UP_DIRECTION;
+}
+
+void Player::playerMoveDown()
+{
+	switch (_direction)
+	{
+	case UP_DIRECTION:
+		this->_Node->yaw(Ogre::Degree(180));
+		_upToDown = 1;
+		_leftToDown = 0;
+		_rightToDown = 0;
+		//_isTurningVertical = -1;
+		break;
+	case LEFT_DIRECTION:
+		this->_Node->yaw(Ogre::Degree(90));
+		_upToDown = 0;
+		_leftToDown = 1;
+		_rightToDown = 0;
+		break;
+	case RIGHT_DIRECTION:
+		this->_Node->yaw(Ogre::Degree(-90));
+		_upToDown = 0;
+		_leftToDown = 0;
+		_rightToDown = 1;
+		break;
+	default:
+		break;
+	}
+	_direction = DOWN_DIRECTION;
+}
+
+void Player::updateAnimation(const Ogre::FrameEvent& evt)
+{
+	if (this->_Keyboard->isKeyDown(OIS::KC_UP) || (this->_Keyboard->isKeyDown(OIS::KC_DOWN)) 
+		|| (this->_Keyboard->isKeyDown(OIS::KC_LEFT)) || (this->_Keyboard->isKeyDown(OIS::KC_RIGHT)) )
+	{
+		this->_animationState = this->_Entity->getAnimationState("Walk");
+	}
+	else
+	{
+		this->_animationState = this->_Entity->getAnimationState("Idle2");
+	}
+	this->_animationState->setLoop(true);
+	this->_animationState->setEnabled(true);
+	this->_animationState->addTime(evt.timeSinceLastFrame);
+}
+
+Ogre::Vector2 Player::convertWorldPosToGridPos(Ogre::Vector3 pos)
+{
+	Ogre::Vector2 rtn;
+	rtn.x = pos.x/120 + 12;
+	rtn.y = pos.z/120 + 10;
+	return rtn;
+}
+
+int Player::getPlayerXPos()
+{
+	return _pos.x;
+}
+
+int Player::getPlayerYPos()
+{
+	return _pos.y;
+}
+
+void Player::updatePlayerWithGrid(int gt)
+{
+	switch(gt)
+	{
+	case 0:
+		break;
+	case 4:
+		_moveSpeed = 0;
+		break;
+	default:
+		break;
+	}
+}
---
+++ src/Player.h
@@ -0,0 +1,53 @@
+#ifndef __PLAYER_H__
+#define __PLAYER_H__
+#include "GameObject.h"
+
+#define START_POSITION Ogre::Vector2(20,0)
+
+typedef enum directionType
+{
+	LEFT_DIRECTION = 1,
+	RIGHT_DIRECTION,
+	UP_DIRECTION,
+	DOWN_DIRECTION
+};
+
+class Player : public GameObject
+{
+public:
+	Player(Ogre::Entity* entity,Ogre::SceneNode* node,Ogre::SceneManager *sceneMgr,Ogre::Camera *camera,OIS::Keyboard *keyboard);
+	~Player(void);
+
+	virtual void Start(void);
+	virtual void Update(const Ogre::FrameEvent& evt);
+	int getPlayerXPos();
+	int getPlayerYPos();
+	void updatePlayerWithGrid(int gt);
+private:
+	//member variables
+	Ogre::Real _moveSpeed;  
+	directionType _direction;  
+	Ogre::Vector3 _transVector;
+	int _isTurningVertical;
+	int _isTurningHorizontal;
+
+	int _leftToRight;
+	int _upToDown;
+	int _leftToUp;
+	int _leftToDown;
+	int _rightToUp;
+	int _rightToDown;
+
+	Ogre::Vector2 _pos;
+	Ogre::Vector2 _destinationPos;
+
+	//methods
+	void playerMoveLeft();
+	void playerMoveRight();
+	void playerMoveUp();
+	void playerMoveDown();
+
+	Ogre::Vector2 convertWorldPosToGridPos(Ogre::Vector3 pos);
+	void updateAnimation(const Ogre::FrameEvent& evt);
+};
+#endif
